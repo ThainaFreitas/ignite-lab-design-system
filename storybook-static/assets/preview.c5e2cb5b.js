22caf65c5f2ad9cb5540d809a04d1405a16ec794
Thainá Freitas <thaina.augustafs@outlook.com>
2022-11-22 14:37:50 -0300

    Initial commit:

---
+++ storybook-static/assets/preview.c5e2cb5b.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.b56cad3d.js";import"./iframe.20c951f9.js";import"./es.map.constructor.a99861ac.js";import"./es.number.to-fixed.92439e1c.js";import"./index.8af678fc.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.c5e2cb5b.js.map
